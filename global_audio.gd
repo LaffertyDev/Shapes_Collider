8b18e2b64dc75332924ed8ac17abc58c4dc09ed0
Nathan Lafferty <nslafferty@gmail.com>
2021-03-28 17:30:18 -0500

    save settings to file

--- global_audio.gd
+++ global_audio.gd
@@ -6,19 +6,35 @@ var SoundLoudness = 0.5
 
 signal audio_volume_changed(value)
 
+var cfg
+
 func _ready():
-	set_master_percentage(0.5)
-	set_music_percentage(0.5)
-	set_sound_percentage(0.5)
+	cfg = ConfigFile.new()
+	var err = cfg.load("user://settings.cfg")
+	if err == OK:
+		set_master_percentage(cfg.get_value("audio", "master", 0.5))
+		set_music_percentage(cfg.get_value("audio", "music", 0.5))
+		set_sound_percentage(cfg.get_value("audio", "sound", 0.5))
+	else:
+		# couldn't load settings, use sane defaults
+		set_master_percentage(0.5)
+		set_music_percentage(0.5)
+		set_sound_percentage(0.5)
 
 func set_master_percentage(value):
 	MasterLoudness = value
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),linear2db(MasterLoudness))
+	cfg.set_value("audio", "master", MasterLoudness)
+	cfg.save("user://settings.cfg")
 
 func set_music_percentage(value):
 	MusicLoudness = value
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"),linear2db(MusicLoudness))
+	cfg.set_value("audio", "music", MusicLoudness)
+	cfg.save("user://settings.cfg")
 
 func set_sound_percentage(value):
 	SoundLoudness = value
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sounds"),linear2db(SoundLoudness))
+	cfg.set_value("audio", "sound", SoundLoudness)
+	cfg.save("user://settings.cfg")
--- globals.gd
+++ globals.gd
@@ -1,10 +1,21 @@
 extends Node
 
-var CurrentDifficulty = 1
-
+var CurrentDifficulty
 
 signal difficulty_changed(value)
+
+var cfg
+
+func _ready():
+	cfg = ConfigFile.new()
+	var err = cfg.load("user://settings.cfg")
+	if err == OK:
+		set_difficulty(cfg.get_value("game", "difficulty", 1))
+	else:
+		set_difficulty(1)
 
 func set_difficulty(value):
 	CurrentDifficulty = value
 	emit_signal("difficulty_changed", value)
+	cfg.set_value("game", "difficulty", CurrentDifficulty)
+	cfg.save("user://settings.cfg")
